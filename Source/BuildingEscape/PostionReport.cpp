ec55e08fa5ac308fa69a4ff03cb7b242a89a279c
Tsa2341 <alanshema2002@gmail.com>
2022-03-26 20:28:24 +0200

    added a door and rotated it

---
+++ Source/BuildingEscape/PostionReport.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PostionReport.h"
+#include "BuildingEscape.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UPostionReport::UPostionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPostionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectLocation = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp,Warning, TEXT("%s report location for %s"), *ObjectName, *ObjectLocation);
+	
+}
+
+
+// Called every frame
+void UPostionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
